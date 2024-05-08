771bec20f26597d48dcff458730274c3ea925883
romanfeshchak <roman.feshchak@gmail.com>
2024-05-08 20:37:34 +0000

    add main tfs

--- main.tf
+++ main.tf
@@ -9,27 +9,27 @@ module "github_repository" {
 
 
 module "gke_cluster" {
-  source         = "github.com/romanfeshchak/terraform-gke-cluster"
-  GOOGLE_REGION  = var.GOOGLE_REGION
-  GOOGLE_PROJECT = var.GOOGLE_PROJECT
-  GOOGLE_ZONE = var.GOOGLE_ZONE
-  GKE_NUM_NODES  = 2
-  SRVC_JSON = var.SRVC_JSON
-  GKE_CLUSTER_NAME = var.GKE_CLUSTER_NAME
-  GKE_MIN_COUNT = var.GKE_MIN_COUNT
-  GKE_MAX_COUNT = var.GKE_MAX_COUNT
-  GKE_CLUSTER_PODS_IP_RANGE_NAME = var.GKE_CLUSTER_PODS_IP_RANGE_NAME
-  GKE_CLUSTER_PODS_IP_RANGE_CIDR = var.GKE_CLUSTER_PODS_IP_RANGE_CIDR
-  VPC_NAME = var.VPC_NAME
-  GKE_SUBNET_CIDR_BLOCK = var.GKE_SUBNET_CIDR_BLOCK
-  GKE_MACHINE_TYPE = var.GKE_MACHINE_TYPE
-  GKE_SUBNET_NAME = var.GKE_SUBNET_NAME
+  source                             = "github.com/romanfeshchak/terraform-gke-cluster"
+  GOOGLE_REGION                      = var.GOOGLE_REGION
+  GOOGLE_PROJECT                     = var.GOOGLE_PROJECT
+  GOOGLE_ZONE                        = var.GOOGLE_ZONE
+  GKE_NUM_NODES                      = 2
+  SRVC_JSON                          = var.SRVC_JSON
+  GKE_CLUSTER_NAME                   = var.GKE_CLUSTER_NAME
+  GKE_MIN_COUNT                      = var.GKE_MIN_COUNT
+  GKE_MAX_COUNT                      = var.GKE_MAX_COUNT
+  GKE_CLUSTER_PODS_IP_RANGE_NAME     = var.GKE_CLUSTER_PODS_IP_RANGE_NAME
+  GKE_CLUSTER_PODS_IP_RANGE_CIDR     = var.GKE_CLUSTER_PODS_IP_RANGE_CIDR
+  VPC_NAME                           = var.VPC_NAME
+  GKE_SUBNET_CIDR_BLOCK              = var.GKE_SUBNET_CIDR_BLOCK
+  GKE_MACHINE_TYPE                   = var.GKE_MACHINE_TYPE
+  GKE_SUBNET_NAME                    = var.GKE_SUBNET_NAME
   GKE_CLUSTER_SERVICES_IP_RANGE_NAME = var.GKE_CLUSTER_SERVICES_IP_RANGE_NAME
   GKE_CLUSTER_SERVICES_IP_RANGE_CIDR = var.GKE_CLUSTER_SERVICES_IP_RANGE_CIDR
-  GKE_POOL_NAME = var.GKE_POOL_NAME
+  GKE_POOL_NAME                      = var.GKE_POOL_NAME
 
 
-} 
+}
 module "flux_bootstrap" {
   source            = "github.com/romanfeshchak/tf-flux"
   github_repository = "${var.GITHUB_OWNER}/${var.FLUX_GITHUB_REPO}"
@@ -37,7 +37,7 @@ module "flux_bootstrap" {
 }
 
 module "tls_private_key" {
-  source = "github.com/romanfeshchak/tf-tls"
+  source    = "github.com/romanfeshchak/tf-tls"
   algorithm = "RSA"
 }
 
--- variables.tf
+++ variables.tf
@@ -1,30 +1,30 @@
 variable "GOOGLE_REGION" {
-    type = string
-    description = "GCP Region"
+  type        = string
+  description = "GCP Region"
 }
 variable "GOOGLE_PROJECT" {
-    type = string
-    description = "GCP project"
+  type        = string
+  description = "GCP project"
 }
 
 variable "GITHUB_OWNER" {
-    type = string
-    description = "GitHub User"
+  type        = string
+  description = "GitHub User"
 }
 
 variable "GITHUB_TOKEN" {
-    type = string
-    description = "GitHub access token"
+  type        = string
+  description = "GitHub access token"
 }
 
 variable "FLUX_GITHUB_REPO" {
-    type = string
-    description = "FLuxCD repo"
+  type        = string
+  description = "FLuxCD repo"
 }
 
 variable "GOOGLE_ZONE" {
-    type = string
-    description = "GCP project"
+  type        = string
+  description = "GCP project"
 }
 
 variable "SRVC_JSON" {
