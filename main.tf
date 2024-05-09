5afbfd5fb3951840f7d15991618eaa3fef09fc83
romanfeshchak <roman.feshchak@gmail.com>
2024-05-09 20:14:16 +0000

    fix tf

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ module "gke_cluster" {
 
 
 }
-module "flux_bootstrap" "terraincognitus" {
+module "flux_bootstrap" {
   source            = "github.com/romanfeshchak/tf-flux"
   github_repository = "${var.GITHUB_OWNER}/${var.FLUX_GITHUB_REPO}"
   private_key       = module.tls_private_key.private_key_pem
